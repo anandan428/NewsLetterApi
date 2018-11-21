6d8fe415e1549461b6193e8e26b715e4f2834dcc
Nandan A <nandan.a@volvo.com>
2018-11-21 16:58:14 +0530

    added deployment folder

--- config/env/production.js
+++ config/env/production.js
@@ -2,7 +2,7 @@ var defaultConfig = require('./default');
 
 module.exports = {
     db: {
-        uri: process.env.MONGODB_URI || 'mongodb://newsletter-db:cmnEikMUCuVrUt3wVcQjFMtJaY43sbIzcDoCzwqUPVpDmprgWGVSpRiLsa1khAyF0zN2st0MMDiIYoJ519rtVA==@newsletter-db.documents.azure.com:10255/?ssl=true',
+        uri: process.env.MONGODB_URI || 'mongodb://newsletter1-db:6dysjLZIMBlud80euhOPsE4kKJ9wceBUujbMOoQH2gAPBQEH182wAz3fgNMWQU4Q4ADnipV5OGzeBFFqvbsVoQ==@newsletter1-db.documents.azure.com:10255/?ssl=true',
         options: {
             useNewUrlParser: true
         },
